6140151b39150bceff1cf7dbc2e3a7ce7e0c3cda
Sk Aminur Rahman <iamaminurrahman2003@gmail.com>
2026-03-18 15:18:33 +0530

    Final Before Whatsapp bot

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_+4ny7x!y7i-c8j^6m9*f2#ji)!!@2%l)1vt6@%gyz!o=l^oa_'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
